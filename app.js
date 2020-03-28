b6d124c232a51bd8f2d96579ec4ec0f553e3f8de
Andrija <andrija@pointjupiter.com>
2020-03-28 14:57:55 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+'strict mode'
+const fetch = require('node-fetch')
+
+const [,, ...args] = process.argv
+
+const body = {
+  'url': args[0]
+}
+
+fetch('https://rel.ink/api/links/', {
+  method: 'POST',
+  body: JSON.stringify(body),
+  headers: { 'Content-Type': 'application/json'}
+})
+  .then(res => res.json())
+  .then(json => console.log(`https://rel.ink/${json.hashid}`))
